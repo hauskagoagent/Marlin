21d6b8a3bcb3a4f1bed75a4f02e88b867bb963e2
hauskagoagent <hauskagoagent@gmail.com>
2020-04-07 15:53:04 +0300

    increased extruder max feedrate to 80mm/s

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -742,7 +742,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 150, 150, 2, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 150, 150, 2, 80 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
